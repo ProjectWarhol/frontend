9e3fc9f996470508aee7dc5e574dc2a725dff6f7
Maurice <maurice.gerhardt@code.berlin>
2022-02-18 10:13:36 +0100

    feat: add solhint and apply changes to Migrations.sol

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.12 <0.9.0;
+pragma solidity ^0.8.12;
 
 contract Migrations {
   address public owner = msg.sender;
-  uint public last_completed_migration;
+  uint public lastCompletedMigration;
+  uint public test;
 
   modifier restricted() {
     require(
       msg.sender == owner,
-      "This function is restricted to the contract's owner"
+      "Function is restricted to owner"
     );
     _;
   }
 
   function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
+    lastCompletedMigration = completed;
   }
 }
