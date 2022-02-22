9a9894c9bc3b74ca29300000efebb587dac9dfc1
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-02-22 22:19:20 +0100

    Run prettier

--- src/utils/NewWallet.js
+++ src/utils/NewWallet.js
@@ -1,11 +1,11 @@
 import { ethers } from 'ethers'
 // eslint-disable-next-line no-unused-vars
 const createWallet = () => {
-  const wallet = ethers.Wallet.createRandom()
-  let data = {}
-  data = {
-    'privateKey':wallet.privateKey,
-    'mnemonicPhrase':wallet.mnemonic.phrase
-  }
-  return data
+    const wallet = ethers.Wallet.createRandom()
+    let data = {}
+    data = {
+        privateKey: wallet.privateKey,
+        mnemonicPhrase: wallet.mnemonic.phrase,
+    }
+    return data
 }
