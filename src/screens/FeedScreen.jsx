8c3e0f2a81af4dc8e3e208199d5352a246d55494
juro95 <julianromer@julians-MacBook-Pro-4.local>
2022-02-23 17:49:36 +0100

    feature/navigation-screens

---
+++ src/screens/FeedScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, StyleSheet } from 'react-native'
+
+const FeedScreen = () => {
+  return <Text style={styles.text}>FeedScreen</Text>
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+})
+
+export default FeedScreen
---
+++ src/screens/ForgotPasswordScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, StyleSheet } from 'react-native'
+
+const ForgotPasswordScreen = () => {
+  return <Text style={styles.text}>forgot screen</Text>
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+})
+
+export default ForgotPasswordScreen
