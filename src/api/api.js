f2d4926aca5c1813b7b1e9305c117c34fb538bb5
juro95 <julianromer@julians-MBP-4.fritz.box>
2022-02-25 10:01:23 +0100

    fix: fixed api

---
+++ src/api/api.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export default axios.create({
+  baseURL: 'http://67e4-185-199-104-14.ngrok.io'
+})
--- src/screens/SignupScreen.jsx
+++ src/screens/SignupScreen.jsx
@@ -1,35 +1,47 @@
-import React, {useState} from 'react'
-import {  StyleSheet, View } from 'react-native'
+import React, {useState, useContext} from 'react'
+import {  StyleSheet, View, ViewBase } from 'react-native'
 import { Text, Input, Button } from 'react-native-elements'
 import Spacer from '../components/Spacer'
+import { Context as AuthContext } from '../context/AuthContext'
 
 const SignupScreen = ( {navigation }) => {
+  const {state, signup } = useContext(AuthContext)
   const[email, setEmail] = useState('')
   const[password, setPassword] = useState('')
+  const[userName, setUsername] = useState('')
 
 
-  return (<View style={styles.container}>
-  <Spacer/>
-  <Text h3>Signup Screen</Text>
-  <Spacer/>
-  <Input
-  label='Email'
-  value={email}
-  onChangeText={setEmail}
-  autoCapitalize='none'
-  autoCorrect={false}
-  />
-  <Spacer/>
-  <Input
-  secureTextEntry
-  label='Password'
-  value={password}
-  onChangeText={setPassword}
-  autoCapitalize='none'
-  autoCorrect={false}
-  />
-  <Spacer/>
-  <Button title='Sign Up' onPress={() => navigation.navigate('signin')} />
+  return (
+  <View style={styles.container}>
+    <Spacer/>
+      <Text h3>Signup Screen</Text>
+    <Spacer/>
+    <Input
+      label='Username'
+      value={userName}
+      onChangeText={setUsername}
+      autoCapitalize='none'
+      autoCorrect={false}
+    />
+    <Spacer/>
+    <Input
+      label='Email'
+      value={email}
+      onChangeText={setEmail}
+      autoCapitalize='none'
+      autoCorrect={false}
+    />
+    <Spacer/>
+    <Input
+      secureTextEntry
+      label='Password'
+      value={password}
+      onChangeText={setPassword}
+      autoCapitalize='none'
+      autoCorrect={false}
+    />
+  {state.errorMessage ? (<Text style={styles.errorMessage}>{state.errorMessage}</Text> ) : null}
+    <Button title='Sign Up' onPress={() => signup({ userName, email, password })} />
   </View>
   )
 }
@@ -42,6 +54,11 @@ const styles = StyleSheet.create({
       marginLeft: 25,
       marginRight: 25,
       marginTop: 50
+    },
+    errorMessage: {
+      fontSize: 16,
+      color: 'red',
+      marginBottom: 20
     }
 })
 
