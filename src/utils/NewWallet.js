6703bde015488707bf7c804e48da0c583b164e74
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2022-02-25 09:38:59 +0100

    Create generateSeedPhrase func

--- src/utils/NewWallet.js
+++ src/utils/NewWallet.js
@@ -1,11 +1,9 @@
-const { ethers } = require('ethers')
+const Web3 = require('web3')
+var bip39 = require('bip39')
+
+const web3 = new Web3()
 // eslint-disable-next-line no-unused-vars
-const createWallet = () => {
-    const wallet = ethers.Wallet.createRandom()
-    let data = {}
-    data = {
-        privateKey: wallet.privateKey,
-        mnemonicPhrase: wallet.mnemonic.phrase,
-    }
-    return data
+const generateSeedPhrase = () => {
+    mnemonic = bip39.generateMnemonic()
+    return mnemonic
 }
