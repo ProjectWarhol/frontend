40ee8effe1d3ed6723faf693bf454c2587fe1966
juro95 <julianromer@julians-MacBook-Pro-4.local>
2022-03-03 12:21:28 +0100

    fix: fixed error showing when changing screens

--- src/screens/SelectAuthenticationScreen.jsx
+++ src/screens/SelectAuthenticationScreen.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {  StyleSheet, View } from 'react-native'
 import { Text, Button } from 'react-native-elements'
 import Spacer from '../components/Spacer'
+import { Context as AuthContext } from '../context/AuthContext'
+import { NavigationEvents } from 'react-navigation'
 
 const SelecteAuthenticationScreen = ({navigation}) => {
+  const { clearErrorMessage } = useContext(AuthContext)
+
   return (
   <View style={styles.container}>
+  <NavigationEvents
+  onWillBlur={clearErrorMessage}
+  />
   <View style={styles.headerContainer}>
   <Text style={styles.text} h3>Welcome</Text>
   </View>
